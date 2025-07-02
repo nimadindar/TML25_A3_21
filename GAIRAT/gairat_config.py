9beda6877663fa8af422e1ad22f2ca6713963e10
nimadindar <nima.dindarsafa@gmail.com>
2025-07-02 15:45:07 +0200

    eps: 0.031 -> 0.015, step_size: 0.007 -> 0.004

--- GAIRAT/gairat_config.py
+++ GAIRAT/gairat_config.py
@@ -4,9 +4,9 @@ class GAIRATconfig:
     EPOCHS = 120
     WEIGHT_DECAY = 2e-4
     MOMENTUM = 0.9
-    EPSILON = 0.031
+    EPSILON = 0.015 # Changed epsilon from 0.031 to 0.015
     NUM_STEPS = 10
-    STEP_SIZE = 0.007
+    STEP_SIZE = 0.004 # Changed step size from 0.007 to 0.004
     SEED = 1
     MODEL = "resnet50"
     DATASET = "task" # Choices: ['task', 'cifar10']
